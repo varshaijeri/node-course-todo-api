6353cc60fea330ea17c8698279f5d68659c7b492
Varsha Andaneppa Ijeri <vijeri@corp.broadsoft.com>
2017-09-05 09:58:32 +0530

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,13 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} =require('./../models/todo');
 
+
+var todosToAdd = [{text:"Get chickens"},{text:"Get car"}];
 //executes before your test case executes
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todosToAdd);
+    }).then(()=>done());
 });
 describe('POST /todos',()=>{
     it('should create a new todo',(done)=>{
@@ -16,14 +20,16 @@ describe('POST /todos',()=>{
             .post('/todos')
             .send({text})
             .expect(200)
+            //response from the UI 
             .expect((res)=>{
                 expect(res.body.text).toBe(text);
             })
+            //to check whether added to DB
             .end((err,res)=>{
                 if(err){
                     return done(err);
                 }
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +49,22 @@ describe('POST /todos',()=>{
                     return done(err);
                 }
                 Todo.find().then((todos)=>{
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e)=>done())
             });
     })
+});
+
+describe('GET /todos',()=>{
+    it('Should get all todos',(done)=>{
+        request(app)
+            .get('/todos')
+            .expect(200)
+            //response from the UI 
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
