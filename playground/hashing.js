3ad0bbfa5cf6bfda75bc3560c0ed59dfd0e8900c
Varsha Andaneppa Ijeri <vijeri@corp.broadsoft.com>
2017-09-06 14:50:00 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,30 @@
 //used only for encryting not decryting
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password ="123abc";
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash)
+//     })
+// });
+
+var hashedPassword = '$2a$10$zHl0fe7BJ.7t3NU1qsq9ZuglysmvAT4LG7G8tW20FrdFqNlfU6JLe';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+})
 
 //2 methods to use sign and verify
 
-var data = {
-    id:10
-};
+// var data = {
+//     id:10
+// };
 
-var token = jwt.sign(data,'salt');
-var decoded = jwt.verify(token,"salt");
-console.log(token);
-console.log(decoded)
+// var token = jwt.sign(data,'salt');
+// var decoded = jwt.verify(token,"salt");
+// console.log(token);
+// console.log(decoded)
 //concept for jwt
 // var msg = "I am varsha";
 // var hash = SHA256(msg);
